70cb3ec22937b10ab9090eefaaeecd710c426af4
abel <raojianli66@163.com>
2020-06-25 14:40:28 +0800

    fix

--- service/tools.go
+++ service/tools.go
@@ -4,5 +4,5 @@ import "time"
 
 // 毫秒转换为 时间
 func millisecondToTime(t int64) time.Time {
-	return time.Unix(t/1000, t%(1000*1000))
+	return time.Unix(t/1000, (t%1000)*1e6)
 }
--- service/tools_test.go
+++ service/tools_test.go
@@ -8,6 +8,9 @@ import (
 
 func TestS(t *testing.T){
 	now := time.Now()
-	toTime := millisecondToTime(now.UnixNano()/1e6)
+	i := now.UnixNano() / 1e6
+	toTime := millisecondToTime(i)
+	unix := time.Unix(i/1000, (i%1000)*1e9)
 	fmt.Println(now,toTime)
+	fmt.Println(unix)
 }
