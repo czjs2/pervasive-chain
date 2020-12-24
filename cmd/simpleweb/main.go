1242f7b6be4cfc1ee789db503b4460b8cbfb698d
abel <raojianli66@163.com>
2020-12-24 15:55:47 +0800

    更新测试

--- cmd/simpleweb/main.go
+++ cmd/simpleweb/main.go
@@ -2,28 +2,33 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"net"
 	"net/http"
 	"time"
 )
 
 var host string
 
+type Handler struct {
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
 	flag.StringVar(&host, "host", ":8899", "web host addr")
 	flag.Parse()
 
-	http.HandleFunc("/api/v1.0/block", func(writer http.ResponseWriter, request *http.Request) {
-		writer.WriteHeader(200)
-		//_, err := writer.Write([]byte(`{"code": 200,"message": "OK","data": {}}`))
-		//if err != nil {
-		//	fmt.Printf("write block %v \n", err)
-		//}
-		//fmt.Printf("update block incomming  %v \n", time.Now())
-	})
-	fmt.Printf("web start %v \n", time.Now())
-	err := http.ListenAndServe(host, nil)
+	server := &http.Server{
+		Handler:      &Handler{},
+		ReadTimeout:  20 * time.Second,
+		WriteTimeout: 20 * time.Second,
+	}
+	listen, err := net.Listen("tcp4", host)
 	if err != nil {
 		panic(err)
 	}
+	server.SetKeepAlivesEnabled(false)
+	err = server.Serve(listen)
 }
